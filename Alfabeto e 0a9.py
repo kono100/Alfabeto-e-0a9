8234d94c31dae58ef607ff7139453371b60304f2
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-05-05 04:43:27 -0300

    Add files via upload

---
+++ Alfabeto e 0a9.py
@@ -0,0 +1,14 @@
+print("\n")
+for i in range(97, 123):
+    print(chr(i), end='')
+    # se quiser só minusculo só apagar a linha de codigo de baixo
+    print(chr(i).upper(), end=' ')
+
+print("\n")
+
+# de 0 a 9
+for i in range(48, 58):
+    print(chr(i), end='')
+
+print("\n")
+
